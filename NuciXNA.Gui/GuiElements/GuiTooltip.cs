64ba35be60887ef526d813b260bf5dcb53e965ce
Horatiu Mlendea <mlendea.horatiu@gmail.com>
2018-06-25 13:44:13 +0300

    Fixed and improved the tooltip

--- NuciXNA.Gui/GuiElements/GuiTooltip.cs
+++ NuciXNA.Gui/GuiElements/GuiTooltip.cs
@@ -1,4 +1,8 @@
-﻿namespace NuciXNA.Gui.GuiElements
+﻿using NuciXNA.Graphics.Enumerations;
+using NuciXNA.Gui.Screens;
+using NuciXNA.Primitives;
+
+namespace NuciXNA.Gui.GuiElements
 {
     /// <summary>
     /// Tool tip GUI element.
@@ -11,6 +15,10 @@
         /// <value>The text.</value>
         public string Text { get; set; }
 
+        public int BorderSize { get; set; }
+
+        GuiImage border;
+        GuiImage background;
         GuiText text;
 
         /// <summary>
@@ -20,6 +28,8 @@
         {
             FontName = "MenuFont";
             Visible = false;
+
+            BorderSize = 1;
         }
 
         /// <summary>
@@ -27,9 +37,20 @@
         /// </summary>
         public override void LoadContent()
         {
+            border = new GuiImage
+            {
+                ContentFile = "ScreenManager/FillImage",
+                TextureLayout = TextureLayout.Tile
+            };
+            background = new GuiImage
+            {
+                ContentFile = "ScreenManager/FillImage",
+                TextureLayout = TextureLayout.Tile
+            };
             text = new GuiText
             {
-                Margins = 2
+                Margins = 2,
+                BackgroundColour = Colour.Transparent
             };
 
             base.LoadContent();
@@ -39,7 +60,38 @@
         {
             base.RegisterChildren();
 
+            AddChild(border);
+            AddChild(background);
             AddChild(text);
+        }
+
+        protected override void SetChildrenProperties()
+        {
+            base.SetChildrenProperties();
+
+            if (ScreenLocation.X + Size.Width > ScreenManager.Instance.Size.Width)
+            {
+                Location = new Point2D(Location.X - (ScreenLocation.X + Size.Width - ScreenManager.Instance.Size.Width), Location.Y);
+            }
+            else if (ScreenLocation.X < 0)
+            {
+                Location = new Point2D(Location.X - ScreenLocation.X, Location.Y);
+            }
+
+            if (ScreenLocation.Y + Size.Height > ScreenManager.Instance.Size.Height)
+            {
+                Location = new Point2D(Location.X, Location.Y - (ScreenLocation.Y + Size.Height - ScreenManager.Instance.Size.Height));
+            }
+            else if (ScreenLocation.Y < 0)
+            {
+                Location = new Point2D(Location.X, Location.Y - ScreenLocation.Y);
+            }
+            
+            border.TintColour = ForegroundColour;
+            background.TintColour = BackgroundColour;
+            background.Size = new Size2D(Size.Width - BorderSize * 2, Size.Height - BorderSize * 2);
+            background.Location = new Point2D(border.Location.X + BorderSize, border.Location.Y + BorderSize);
+            text.Text = Text;
         }
     }
 }
