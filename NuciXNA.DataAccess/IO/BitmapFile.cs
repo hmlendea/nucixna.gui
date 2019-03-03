17f8a75d575e7ce90fe84033c5b0a687704f8fee
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-03-03 11:42:16 +0200

    New BitmapFile implementation Replaced System.Drawing with SixLabors.ImageSharp

--- NuciXNA.DataAccess/IO/BitmapFile.cs
+++ NuciXNA.DataAccess/IO/BitmapFile.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 using NuciXNA.Primitives;
 
@@ -10,24 +11,9 @@ namespace NuciXNA.DataAccess.IO
     /// <summary>
     /// Fast bitmap manipulator.
     /// </summary>
-    public class BitmapFile : IDisposable
+    public class Bitmap : IDisposable
     {
-        readonly Bitmap sourceBitmap;
-        IntPtr Iptr = IntPtr.Zero;
-        BitmapData bitmapData;
-        bool bitsLocked;
-
-        /// <summary>
-        /// Gets or sets the pixels.
-        /// </summary>
-        /// <value>The pixels.</value>
-        public byte[] Pixels { get; set; }
-
-        /// <summary>
-        /// Gets the depth.
-        /// </summary>
-        /// <value>The depth.</value>
-        public int Depth { get; private set; }
+        Image<Rgba32> sourceImage;
 
         /// <summary>
         /// Gets the size.
@@ -36,152 +22,47 @@ namespace NuciXNA.DataAccess.IO
         public Size2D Size { get; private set; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
-        /// </summary>
-        /// <param name="sourceBitmap">Source bitmap.</param>
-        public BitmapFile(Bitmap sourceBitmap)
-        {
-            this.sourceBitmap = sourceBitmap;
-            LockBits();
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
+        /// Initializes a new instance of the <see cref="Bitmap"/> class.
         /// </summary>
         /// <param name="sourceImage">Source image.</param>
-        public BitmapFile(Image sourceImage)
+        public Bitmap(Image<Rgba32> sourceImage)
         {
-            sourceBitmap = (Bitmap)sourceImage;
-            LockBits();
+            this.sourceImage = sourceImage;
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
-        /// </summary>
-        /// <param name="fileName">File name.</param>
-        public BitmapFile(string fileName)
-        {
-            sourceBitmap = new Bitmap(fileName);
-            LockBits();
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
-        /// </summary>
-        /// <param name="type">Type.</param>
-        /// <param name="resource">Resource.</param>
-        public BitmapFile(Type type, string resource)
-        {
-            sourceBitmap = new Bitmap(type, resource);
-            LockBits();
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
+        /// Initializes a new instance of the <see cref="Bitmap"/> class.
         /// </summary>
         /// <param name="width">Width.</param>
         /// <param name="height">Height.</param>
-        public BitmapFile(int width, int height)
+        public Bitmap(int width, int height)
         {
-            sourceBitmap = new Bitmap(width, height);
-            LockBits();
+            sourceImage = new Image<Rgba32>(width, height);
+
+            Size = new Size2D(width, height);
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
-        /// </summary>
-        /// <param name="width">Width.</param>
-        /// <param name="height">Height.</param>
-        /// <param name="format">Format.</param>
-        public BitmapFile(int width, int height, PixelFormat format)
-        {
-            sourceBitmap = new Bitmap(width, height, format);
-            LockBits();
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
-        /// </summary>
-        /// <param name="width">Width.</param>
-        /// <param name="height">Height.</param>
-        /// <param name="stride">Stride.</param>
-        /// <param name="format">Format.</param>
-        /// <param name="scan0">Scan0.</param>
-        public BitmapFile(int width, int height, int stride, PixelFormat format, IntPtr scan0)
-        {
-            sourceBitmap = new Bitmap(width, height, stride, format, scan0);
-            LockBits();
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
+        /// Initializes a new instance of the <see cref="Bitmap"/> class.
         /// </summary>
         /// <param name="size">Size.</param>
-        public BitmapFile(Size2D size)
+        public Bitmap(Size2D size)
             : this(size.Width, size.Height) { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
+        /// Initializes a new instance of the <see cref="Bitmap"/> class.
         /// </summary>
-        /// <param name="size">Size.</param>
-        /// <param name="format">Format.</param>
-        public BitmapFile(Size2D size, PixelFormat format)
-            : this(size.Width, size.Height, format) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BitmapFile"/> class.
-        /// </summary>
-        /// <param name="size">Size.</param>
-        /// <param name="stride">Stride.</param>
-        /// <param name="format">Format.</param>
-        /// <param name="scan0">Scan0.</param>
-        public BitmapFile(Size2D size, int stride, PixelFormat format, IntPtr scan0)
-            : this(size.Width, size.Height, stride, format, scan0) { }
-
-        /// <summary>
-        /// Locks the bitmap data
-        /// </summary>
-        public void LockBits()
+        /// <param name="fileName">File name.</param>
+        Bitmap(string fileName)
         {
-            if (bitsLocked)
-            {
-                return;
-            }
+            sourceImage = Image.Load(fileName);
 
-            bitsLocked = true;
-
-            Size = new Size2D(sourceBitmap.Width, sourceBitmap.Height);
-            
-            Rectangle rect = new Rectangle(0, 0, Size.Width, Size.Height);
-
-            Depth = Image.GetPixelFormatSize(sourceBitmap.PixelFormat);
-
-            if (Depth != 8 && Depth != 24 && Depth != 32)
-            {
-                throw new ArgumentException("Only 8, 24 and 32 bpp images are supported.");
-            }
-
-            bitmapData = sourceBitmap.LockBits(rect, ImageLockMode.ReadWrite, sourceBitmap.PixelFormat);
-
-            int step = Depth / 8;
-            Pixels = new byte[(int)Size.Area * step];
-            Iptr = bitmapData.Scan0;
-
-            Marshal.Copy(Iptr, Pixels, 0, Pixels.Length);
+            Size = new Size2D(sourceImage.Width, sourceImage.Height);
         }
 
-        /// <summary>
-        /// Unlocks the bitmap data
-        /// </summary>
-        public void UnlockBits()
+        public static Bitmap Load(string fileName)
         {
-            if (!bitsLocked)
-            {
-                return;
-            }
-
-            Marshal.Copy(Pixels, 0, Iptr, Pixels.Length);
-            sourceBitmap.UnlockBits(bitmapData);
+            return new Bitmap(fileName);
         }
 
         /// <summary>
@@ -192,38 +73,8 @@ namespace NuciXNA.DataAccess.IO
         /// <returns>Pixel colou.r</returns>
         public Colour GetPixel(int x, int y)
         {
-            Colour colour = new Colour();
-            byte a, r, g, b;
-            int colorComponentsCount = Depth / 8;
-            int index = ((y * Size.Width) + x) * colorComponentsCount;
-
-            if (index > Pixels.Length - colorComponentsCount)
-            {
-                throw new IndexOutOfRangeException();
-            }
-
-            switch (Depth)
-            {
-                case 32:
-                    b = Pixels[index];
-                    g = Pixels[index + 1];
-                    r = Pixels[index + 2];
-                    a = Pixels[index + 3];
-                    colour = Colour.FromArgb(a, r, g, b);
-                    break;
-
-                case 24:
-                    b = Pixels[index];
-                    g = Pixels[index + 1];
-                    r = Pixels[index + 2];
-                    colour = Colour.FromArgb(r, g, b);
-                    break;
-
-                case 8:
-                    b = Pixels[index];
-                    colour = Colour.FromArgb(b, b, b);
-                    break;
-            }
+            Rgba32 pixel = sourceImage[x, y];
+            Colour colour = Colour.FromArgb(pixel.A, pixel.R, pixel.G, pixel.B);
 
             return colour;
         }
@@ -246,28 +97,8 @@ namespace NuciXNA.DataAccess.IO
         /// <param name="colour">Pixel colour.</param>
         public void SetPixel(int x, int y, Colour colour)
         {
-            int colorComponentsCount = Depth / 8;
-            int index = ((y * Size.Width) + x) * colorComponentsCount;
-
-            switch (Depth)
-            {
-                case 32:
-                    Pixels[index] = colour.B;
-                    Pixels[index + 1] = colour.G;
-                    Pixels[index + 2] = colour.R;
-                    Pixels[index + 3] = colour.A;
-                    break;
-
-                case 24:
-                    Pixels[index] = colour.B;
-                    Pixels[index + 1] = colour.G;
-                    Pixels[index + 2] = colour.R;
-                    break;
-
-                case 8:
-                    Pixels[index] = colour.B;
-                    break;
-            }
+            Rgba32 pixel = new Rgba32(colour.R, colour.G, colour.B, colour.A);
+            sourceImage[x, y] = pixel;
         }
 
         /// <summary>
@@ -281,40 +112,15 @@ namespace NuciXNA.DataAccess.IO
         }
 
         /// <summary>
-        /// Releases all resource used by the <see cref="BitmapFile"/> object.
+        /// Releases all resource used by the <see cref="Bitmap"/> object.
         /// </summary>
-        /// <remarks>Call <see cref="Dispose"/> when you are finished using the <see cref="BitmapFile"/>. The
-        /// <see cref="Dispose"/> method leaves the <see cref="BitmapFile"/> in an unusable state. After
-        /// calling <see cref="Dispose"/>, you must release all references to the <see cref="BitmapFile"/>
-        /// so the garbage collector can reclaim the memory that the <see cref="BitmapFile"/> was occupying.</remarks>
+        /// <remarks>Call <see cref="Dispose"/> when you are finished using the <see cref="Bitmap"/>. The
+        /// <see cref="Dispose"/> method leaves the <see cref="Bitmap"/> in an unusable state. After
+        /// calling <see cref="Dispose"/>, you must release all references to the <see cref="Bitmap"/>
+        /// so the garbage collector can reclaim the memory that the <see cref="Bitmap"/> was occupying.</remarks>
         public void Dispose()
         {
-            UnlockBits();
-            sourceBitmap.Dispose();
-        }
-
-        /// <param name="source">Source <see cref="BitmapFile"/>.</param>
-        public static implicit operator Bitmap(BitmapFile source)
-        {
-            return source.sourceBitmap;
-        }
-
-        /// <param name="source">Source <see cref="Bitmap"/>.</param>
-        public static implicit operator BitmapFile(Bitmap source)
-        {
-            return new BitmapFile(source);
-        }
-
-        /// <param name="source"><see cref="BitmapFile"/>.</param>
-        public static implicit operator Image(BitmapFile source)
-        {
-            return source.sourceBitmap;
-        }
-
-        /// <param name="source">Source <see cref="Image"/>.</param>
-        public static implicit operator BitmapFile(Image source)
-        {
-            return new BitmapFile(source);
+            sourceImage.Dispose();
         }
     }
 }
