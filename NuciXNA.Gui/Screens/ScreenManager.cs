6c52109e84c55377063cd6de36c0b484c3de4f90
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-03-07 12:36:26 +0200

    Generic overloads for the ChangeScreens method

--- NuciXNA.Gui/Screens/ScreenManager.cs
+++ NuciXNA.Gui/Screens/ScreenManager.cs
@@ -139,28 +139,32 @@ namespace NuciXNA.Gui.Screens
             }
         }
 
-        /// <summary>
-        /// Changes the screen.
-        /// </summary>
-        /// <param name="screenType">Screen type.</param>
-        public void ChangeScreens(Type screenType)
-        {
-            ChangeScreens(screenType, null);
-        }
+        public void ChangeScreens<TScreen>()
+            => ChangeScreens<TScreen>(null);
+
+        public void ChangeScreens<TScreen>(params object[] parameters)
+            => ChangeScreens(typeof(TScreen), parameters);
 
         /// <summary>
         /// Changes the screen.
         /// </summary>
         /// <param name="screenType">Screen type.</param>
-        /// <param name="screenArgs">Screen arguments.</param>
-        public void ChangeScreens(Type screenType, params object[] screenArgs)
+        public void ChangeScreens(Type screenType)
+            => ChangeScreens(screenType, null);
+
+        /// <summary>
+        /// Changes the screen.
+        /// </summary>
+        /// <param name="screenType">Screen type.</param>
+        /// <param name="parameters">Screen parameters.</param>
+        public void ChangeScreens(Type screenType, params object[] parameters)
         {
             if (Transitioning)
             {
                 return;
             }
             
-            newScreen = (Screen)Activator.CreateInstance(screenType, screenArgs);
+            newScreen = (Screen)Activator.CreateInstance(screenType, parameters);
 
             if (currentScreen == null)
             {
